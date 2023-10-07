2ac155d62317ced3ce0c79ae1b7539915391222c
NithishASU <nsarava3@asu.edu>
2023-10-07 13:29:07 -0700

    update main.js

--- main.js
+++ main.js
@@ -77,11 +77,7 @@ function connectDeviceAndCacheCharacteristic(device) {
         characteristicCache = characteristic;
 
         return characteristicCache;
-      }).
-      then(characteristic => {
-        characteristic.addEventListener('characteristicvaluechanged',
-                                        handleCharacteristicValueChanged);
-        console.log('Notifications have been started.');
+      });
 }
 // Enable the characteristic changes notification
 function startNotifications(characteristic) {
